daaa9ccd4f37cd5b135fac229a2acd2c9ab16f4b
Artem Kulikov <chigar2010@yandex.ru>
2024-05-07 00:17:12 +0200

    update

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,12 +1,13 @@
 def multiplication_table(number: int) -> None:
     table = [['' for _ in range(number + 1)] for _ in range(number + 1)]
+    len_max_num = len(str(number * number))
     for row in range(number + 1):
-        for colum in range(number + 1):
-            if row == 0 or colum == 0:
-                table[row][colum] = str(row + colum)
+        for column in range(number + 1):
+            if row == 0 or column == 0:
+                table[row][column] = str(row + column).rjust(len_max_num)
             else:
-                table[row][colum] = str(row * colum)
-    table[0][0] = ' '
+                table[row][column] = str(row * column).rjust(len_max_num)
+    table[0][0] = ''.rjust(len_max_num)
     for row in range(number + 1):
         print(' '.join(table[row]))
 
